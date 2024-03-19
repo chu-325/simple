be861a76ae0ed59e1cf569e7ce80359ef96abcc7
leader <chunanholmes@gmail.com>
2024-03-19 11:51:10 +0700

    euler

---
+++ calulate /euler.py
@@ -0,0 +1,20 @@
+def main():
+    n = int(input(" e(n) , n = "))
+    print("euler({}) = {:.5f}".format(n, euler(n)))
+
+def fact(k) :
+    if k <= 1 :
+        return 1
+    else:
+        return k * fact(k - 1)
+
+
+def euler(n) :
+    if n <= 0 :
+        return 1
+    else:
+        return 1/(fact(n)) + euler(n - 1)
+
+
+if __name__ == "__main__":
+    main()
